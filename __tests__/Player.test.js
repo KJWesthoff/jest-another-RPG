cc8133259951089ee703ad37b4c1357c104fa7a1
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-17 13:52:58 -0800

    potion bookkeeping

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -62,3 +62,30 @@ test("reduce health function", () =>{
 
 
 });
+
+
+test("gets players attack value" , () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('add potion to the inventory', () =>{
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+
+    player.addPotion(new Potion());
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+
+test('use a potion from the inventory', ()=> {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
