6e47950e38890c5a81a90357137685c36c626aaa
Karl-Johan Westhoff <karl.johan.westhoff@gmail.com>
2020-11-17 13:23:57 -0800

    remove and check health

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -34,4 +34,31 @@ test('get inventory test i should return a list or false', ()=>{
     expect(player.getInventory()).toEqual(expect.any(Array));
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
-})
+});
+
+test("get player health", ()=>{
+    const player = new Player("Dave");
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+test("is player alive", () =>{
+    const player = new Player("Dave");
+
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+
+test("reduce health function", () =>{
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth-5);
+
+    player.reduceHealth(9999);
+    expect(player.health).toBe(0);
+
+
+});
